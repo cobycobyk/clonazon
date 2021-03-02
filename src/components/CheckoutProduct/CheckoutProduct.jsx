b45ead2939f3d33da9e44193386b465278d2eb57
cobycobyk <coby@robo3d.com>
2021-03-02 11:41:41 -0700

    add remove from basket functionality

--- src/components/CheckoutProduct/CheckoutProduct.jsx
+++ src/components/CheckoutProduct/CheckoutProduct.jsx
@@ -1,6 +1,17 @@
+import { useStateValue } from "../../StateProvider";
 import "./CheckoutProduct.css";
 
 export default function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    //remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -18,7 +29,7 @@ export default function CheckoutProduct({ id, image, title, price, rating }) {
               <p>⭐</p>
             ))}
         </div>
-        <button>Remove from Cart</button>
+        <button onClick={removeFromBasket}>Remove from Cart</button>
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -8,14 +8,29 @@ export const getBasketTotal = (basket) =>
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TO_BASKET' :
+    case 'ADD_TO_BASKET':
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
 
-      default:
-        return state;
+    case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+
+      } else {
+        console.warn(`Cannot remove id ${action.id} as it is not in the cart!`)
+      }
+
+      return {...state, basket: newBasket}
+
+    default:
+      return state;
   }
 }
 
