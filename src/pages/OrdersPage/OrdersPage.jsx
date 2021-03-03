5f2102fc1ed1bc6abfa422d4e89110c706014c69
cobycobyk <coby@robo3d.com>
2021-03-02 18:18:25 -0700

    orders page

--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -30,10 +30,11 @@ export default function OrdersPage() {
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+
       <div className="orders__order">
-        {orders?.map((order) => {
-          <Order order={order} />;
-        })}
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
       </div>
     </div>
   );
