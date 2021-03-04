3260f65c0c884752dadc63b4fbba03a29cb433a7
cobycobyk <coby@robo3d.com>
2021-03-04 10:13:26 -0700

    firebase bug fix, add favicon and title

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-cc6c4/us-central1/api' // the api url
+  baseURL: 'https://us-central1-clone-cc6c4.cloudfunctions.net/api' // the api url
+  //'http://localhost:5001/clone-cc6c4/us-central1/api'
 });
 
 export default instance;
